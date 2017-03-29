26570834c162d6c0541a6fac12bf219fd97d1e6b
caoxuedong <catcher92@163.com>
2017-03-29 13:40:38 +0800

    初始化javademo

---
+++ src/main/java/com/catcher92/demo/App.java
@@ -0,0 +1,13 @@
+package com.catcher92.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
